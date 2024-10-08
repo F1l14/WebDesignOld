812836f6039cc8fa1b767e557ff36970091adfa4
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-10-08 20:23:26 +0300

    change in error catching

--- dbconn.php
+++ dbconn.php
@@ -1,15 +1,17 @@
 <?php
-    $db_server="localhost";
-    $db_user="root";
-    $db_pass="";
-    $db_name="ndp";
-    $conn="";
-
-    try{
-        $conn= mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-        echo "Conenction to Database Successful" . "<br>";
-    }catch (mysqli_sql_exception){
-        echo "Connection to Database Failed" . "<br>";
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "ndp";
+    
+    // Try to establish a connection
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    
+    // Check if the connection was successful
+    if ($conn) {
+        echo "Connection to Database Successful" . "<br>";
+    } else {
+        echo "Connection to Database Failed: " . mysqli_connect_error() . "<br>";
     }
-   
 ?>
+    
