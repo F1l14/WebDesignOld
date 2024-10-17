4be2a26ed1e6032e3e4ae415769ff6822d6797a8
F1l14 <filipmn42@gmail.com>
2024-10-17 17:31:20 +0300

    basic menu

--- homepage.php
+++ homepage.php
@@ -8,7 +8,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
-
+    <?php include("header.html")?>
+    <link rel="stylesheet" href="style.css">
 This is the Homepage
 <hr>
 </head>
@@ -16,6 +17,30 @@ This is the Homepage
     <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
         <input type="submit" name="logout" value="Logout">
     </form>
+
+    <article>
+        <h2>Items</h2>
+        <ol>
+            <li>bread</li>
+            <li>spoons
+                <ul>
+                    <li>long</li>
+                    <li>short</li>
+                </ul>
+            </li>
+            <li>food</li>
+        </ol>
+    </article>
+
+
+    <article>
+        <h2>Personnel</h2>
+        <ul>
+            <li>James</li>
+            <li>Jonathan</li>
+            <li>Lilly</li>
+        </ul>
+    </article>
 </body>
 </html>
 <?php
