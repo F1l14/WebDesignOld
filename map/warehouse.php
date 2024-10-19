e57474d2d2bc9461e1fe3ea08fc2e8bc8c455815
kostisalexas <konstantinosa57@gmail.com>
2024-10-19 19:21:28 +0300

    Προβολή Κατάστασης Αποθήκης

---
+++ map/warehouse.php
@@ -0,0 +1,30 @@
+// warehouse.php
+//Δημιουργία πίνακα που εμφανίζει τα είδη αποθήκης
+
+<table>
+    <thead>
+        <tr>
+            <th>Είδος</th>
+            <th>Κατηγορία</th>
+            <th>Ποσότητα στη Βάση</th>
+            <th>Ποσότητα σε Οχήματα</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        include("dbconn.php");
+        $sql = "SELECT * FROM items";
+        $result = $conn->query($sql);
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['item_name'] . "</td>";
+            echo "<td>" . $row['category'] . "</td>";
+            echo "<td>" . $row['quantity_base'] . "</td>";
+            echo "<td>" . $row['quantity_vehicle'] . "</td>";
+            echo "</tr>";
+        }
+        $conn->close();
+        ?>
+    </tbody>
+</table>
