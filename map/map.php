db0c303bf4194002cd6e51661df4ac2bb1839185
kostisalexas <konstantinosa57@gmail.com>
2024-10-19 18:23:43 +0300

    fetch load markers and display them to the map

--- map/map.php
+++ map/map.php
@@ -20,5 +20,40 @@
     <div id="map">
         <!-- Κώδικας για τον διαδραστικό χάρτη -->
     </div>
+    
+    <script>
+    // Φόρτωση markers από τον server
+    function loadMarkers() {
+        fetch('../loadMarkers.php')
+            .then(response => response.json())
+            .then(data => {
+                // Εμφάνιση οχημάτων
+                data.vehicles.forEach(vehicle => {
+                    L.marker([vehicle.lat, vehicle.lng], {className: 'marker-vehicle'})
+                        .addTo(map)
+                        .bindPopup(`<b>${vehicle.username}</b><br>Κατάσταση: ${vehicle.status}<br>Φορτίο: ${vehicle.cargo}`);
+                });
+
+                // Εμφάνιση αιτημάτων
+                data.requests.forEach(request => {
+                    L.marker([request.lat, request.lng], {className: 'marker-request'})
+                        .addTo(map)
+                        .bindPopup(`<b>${request.name}</b><br>Τηλέφωνο: ${request.phone}<br>Ποσότητα: ${request.quantity}`);
+                });
+
+                // Εμφάνιση προσφορών
+                data.offers.forEach(offer => {
+                    L.marker([offer.lat, offer.lng], {className: 'marker-offer'})
+                        .addTo(map)
+                        .bindPopup(`<b>${offer.name}</b><br>Τηλέφωνο: ${offer.phone}<br>Ποσότητα: ${offer.quantity}`);
+                });
+            })
+            .catch(error => console.log('Error loading markers:', error));
+    }
+
+    // Κλήση της λειτουργίας κατά την έναρξη
+    loadMarkers();
+</script>
+
 </body>
 </html>
