00e0cbffe0243e2400228ce70281eea05e00feb1
kostisalexas <konstantinosa57@gmail.com>
2024-10-19 19:25:56 +0300

    Δημιουργία Accounts Διασωστών

---
+++ map/create_rescuer.php
@@ -0,0 +1,10 @@
+// create_rescuer.php
+// Φόρμα για δημιουργία νέων λογαριασμών διασωστών.
+
+<form action="create_rescuer_action.php" method="POST">
+    <label for="username">Username:</label>
+    <input type="text" name="username" required>
+    <label for="password">Password:</label>
+    <input type="password" name="password" required>
+    <button type="submit">Create Account</button>
+</form>
